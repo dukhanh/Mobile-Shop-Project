bc90a7e83c6c1cd9dac4ede83d92d78212c798c5
dukhanh <18130108@st.hcmuaf.edu.vn>
2022-05-10 14:29:32 +0700

    delete cookies when logout

--- src/main/java/controller_account/LogoutController.java
+++ src/main/java/controller_account/LogoutController.java
@@ -11,6 +11,20 @@ public class LogoutController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.removeAttribute("account");
+
+        Cookie[] arr = request.getCookies();
+        if (arr != null) {
+            for (Cookie c : arr) {
+                if (c.getName().equals("email")) {
+                   c.setMaxAge(0);
+                   response.addCookie(c);
+                }
+                if (c.getName().equals("password")) {
+                    c.setMaxAge(0);
+                    response.addCookie(c);
+                }
+            }
+        }
         response.sendRedirect("home");
     }
 
